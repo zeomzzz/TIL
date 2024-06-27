e16a93156183ef52585d839c9383cb3b31843f57
zeomzzz <jmlee2705@gmail.com>
2024-06-28 01:14:39 +0900

    feat: 240627

--- Language/C/nadocoding-c/MyProject/function.c
+++ Language/C/nadocoding-c/MyProject/function.c
@@ -7,7 +7,13 @@ void function_without_return();
 int function_with_return();
 void function_without_params();
 void function_with_params(int num1, int num2, int num3);
+
 int apple(int total, int ate); // total개에서 ate개 먹고 남은 수 반환
+
+int add(int num1, int num2);
+int sub(int num1, int num2);
+int mul(int num1, int num2);
+int div(int num1, int num2);
 
 int main(void)
 {
@@ -18,44 +24,56 @@ int main(void)
     // printf("numdms %d 입니다\n", num);
     p(num);
 
-    // 2+3
-    num = num + 3;
-    // printf("nun은 %d입니다\n", num); // 5
+    num = add(num, 3);
     p(num);
 
-    // 5-1
-    num -= 1;
-    // printf("num은 %d입니다\n", num);
+    num = sub(num, 1);
     p(num);
 
-    // 4*3
-    num *= 3;
-    // printf("num은 %d 입니다\n", num);
+    num = mul(num, 3);
     p(num);
 
-    // 12 / 6
-    num /= 6;
-    // printf("num은 %d입니다\n", num);
+    num = div(num, 6);
     p(num);
 
-    // 함수 종류
-    // 반환값이 없는 함수
-    function_without_return();
+    // // 2+3
+    // num = num + 3;
+    // // printf("nun은 %d입니다\n", num); // 5
+    // p(num);
 
-    // 반환값이 있는 함수
-    int ret = function_with_return();
-    p(ret);
+    // // 5-1
+    // num -= 1;
+    // // printf("num은 %d입니다\n", num);
+    // p(num);
 
-    // 전달값이 없는 함수
-    function_without_params();
+    // // 4*3
+    // num *= 3;
+    // // printf("num은 %d 입니다\n", num);
+    // p(num);
 
-    // 전달값이 있는 함수
-    function_with_params(1, 2, 3);
+    // // 12 / 6
+    // num /= 6;
+    // // printf("num은 %d입니다\n", num);
+    // p(num);
 
-    // 반환값과 전달값이 있는 함수
-    int ret2 = apple(5, 2); // 5개의 사과 중에서 2개를 먹음
-    printf("사과 5개 중에 2개를 먹으면 %d개가 남아요\n", ret2);
-    printf("사과 %d개 중에 %d개를 먹으면 %d개가 남아요\n", 10, 4, apple(10, 4));
+    // // 함수 종류
+    // // 반환값이 없는 함수
+    // function_without_return();
+
+    // // 반환값이 있는 함수
+    // int ret = function_with_return();
+    // p(ret);
+
+    // // 전달값이 없는 함수
+    // function_without_params();
+
+    // // 전달값이 있는 함수
+    // function_with_params(1, 2, 3);
+
+    // // 반환값과 전달값이 있는 함수
+    // int ret2 = apple(5, 2); // 5개의 사과 중에서 2개를 먹음
+    // printf("사과 5개 중에 2개를 먹으면 %d개가 남아요\n", ret2);
+    // printf("사과 %d개 중에 %d개를 먹으면 %d개가 남아요\n", 10, 4, apple(10, 4));
 
     return 0;
 }
@@ -91,4 +109,24 @@ int apple(int total, int ate)
 {
     printf("전달값과 반환값이 있는 함수입니다.\n");
     return total - ate;
+}
+
+int add(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int sub(int num1, int num2)
+{
+    return num1 - num2;
+}
+
+int mul(int num1, int num2)
+{
+    return num1 * num2;
+}
+
+int div(int num1, int num2)
+{
+    return num1 / num2;
 }
